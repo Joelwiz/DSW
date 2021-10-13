8d3f67f06f51dff0b7faeee25871981d338c7552
Joelwiz <51479918+Joelwiz@users.noreply.github.com>
2021-10-13 11:09:38 +0100

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index2</title>
+
+
+</head>
+<body>
+
+<h2>index2</h2>
+
+</body>
+</html>
